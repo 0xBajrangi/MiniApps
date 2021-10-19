dbdf6fae776668ce9687929fd61df22bc2c33ba2
Pawan Janglu Lode <pawanjlode@gmail.com>
2021-10-19 21:05:43 +0530

    first layout forwit nav

---
+++ FoodApp/component/navbar.js
@@ -0,0 +1,13 @@
+function navbar() {
+    return `<div>
+                <a href="index.html"><h1>Food App</h1></a>
+            </div>
+            <div id="nav-items">
+                <div><input type="text" name="" id="search"></div>
+                <div><a href="recepie.html">Recepie of Day</a></div>
+                <div><a href="Latest.html">Latest Recepie</a></div>
+            </div>`;
+    
+}
+
+export { navbar };
